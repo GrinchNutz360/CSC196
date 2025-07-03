f5b1b879d02c91fbab28af72d649bfb2658c80e0
Tristin <TCturtle16@outlook.com>
2025-07-03 09:56:47 -0600

    Made math, random, and time for engine.

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace viper {
+	constexpr float pi = 3.14159265358979323846f;
+	constexpr float twoPi = pi * 2.0f;
+	constexpr float halfPi = pi / 2.0f;
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,4 +1,8 @@
+#include "Core/Math.h"
+#include "../../Renderer/Renderer.h"
 #include <iostream>
 int main() {
+	viper::PrintMessage();
 	std::cout << "Hello, World!\n";
+	std::cout << viper::pi << std::endl;
 }
