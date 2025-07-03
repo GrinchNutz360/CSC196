0a6ee4dfe6efb5b13db92ddac7a5cbc6985fa0f3
Tristin <TCturtle16@outlook.com>
2025-07-03 10:03:14 -0600

    Forgot to get rid of changes from the  practice renderer file

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,8 +1,7 @@
 #include "Core/Math.h"
-#include "../../Renderer/Renderer.h"
+
 #include <iostream>
 int main() {
-	viper::PrintMessage();
 	std::cout << "Hello, World!\n";
 	std::cout << viper::pi << std::endl;
 }
